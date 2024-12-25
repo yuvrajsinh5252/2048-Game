7531a972f9d9109bab4a84e7bd7369e62b9fe109
Yuvrajsinh Gohil <yuvrajsinh476@gmail.com>
2024-12-25 17:28:34 +0530

    fmt: prettier

--- 2048.py
+++ 2048.py
@@ -1,34 +1,43 @@
 import os
 import random
-from rich.console import Console
-from pynput import keyboard
 
-x = [ [" ", " ", " ", " "], [" ",2," ", " "], [" ", " ", " ", " "],[" ", " ", " ", " "] ]
+from pynput import keyboard
+from rich.console import Console
+
+x = [
+    [" ", " ", " ", " "],
+    [" ", 2, " ", " "],
+    [" ", " ", " ", " "],
+    [" ", " ", " ", " "],
+]
+
 
 def clear():
-    if (os.name == "posix"):
+    if os.name == "posix":
         os.system("clear")
     else:
         os.system("cls")
 
+
 def game_board():
-    print(' -------------------------------')
+    print(" -------------------------------")
     for i in range(4):
         for j in range(5):
-            if (j < 4):
+            if j < 4:
                 l = len(str(x[i][j]))
-            print('|',end="")
-            if (l == 1 or l == 2):
+            print("|", end="")
+            if l == 1 or l == 2:
                 spaces = 3
-            if (l == 3 or l == 4):
+            if l == 3 or l == 4:
                 spaces = 2
-            print(" " * spaces,end="")
-            if (l == 2 or l == 4):
+            print(" " * spaces, end="")
+            if l == 2 or l == 4:
                 spaces -= 1
-            print(f'[bold cyan]{x[i][j] if j < 4 else ""}[/]',end="")
-            print(" " * spaces,end="")
+            print(f'[bold cyan]{x[i][j] if j < 4 else ""}[/]', end="")
+            print(" " * spaces, end="")
         print()
-        print('--------------------------------')
+        print("--------------------------------")
+
 
 clear()
 score = 0
@@ -36,8 +45,8 @@ c = Console()
 print = c.print
 clear()
 
-print("""[bold red]   
- ___ ___ ___ ___ 
+print("""[bold red]
+ ___ ___ ___ ___
 |_  |   | | | . |
 |  _| | |_  | . |
 |___|___| |_|___|
@@ -52,172 +61,181 @@ with keyboard.Events() as events:
 clear()
 game_board()
 
+
 def is_win():
     for i in range(4):
         for j in range(4):
-            if (x[i][j] == 2048):
+            if x[i][j] == 2048:
                 return True
-            
+
+
 def get_max():
     ans = 1
     for i in range(4):
         for j in range(4):
-            if (x[i][j] != " " and ans == 1):
+            if x[i][j] != " " and ans == 1:
                 max = x[i][j]
-                ans = 0 
+                ans = 0
                 break
-            if(x[i][j] != " " ):
-                if (x[i][j] > max):
+            if x[i][j] != " ":
+                if x[i][j] > max:
                     max = x[i][j]
     return max
+
 
 def empty():
     for i in range(4):
         for j in range(4):
-            if (x[i][j] == " "):
+            if x[i][j] == " ":
                 return True
 
+
 def random_num():
-    if (empty()):
+    if empty():
         num = 0
         ans = False
-        index1 = random.randint(0,3)
-        index2 = random.randint(0,3)
+        index1 = random.randint(0, 3)
+        index2 = random.randint(0, 3)
         for i in range(4):
             for j in range(4):
-                if (i == index1 and j == index2 and x[i][j] == " "):
-                    num = random.choice([2,4])
+                if i == index1 and j == index2 and x[i][j] == " ":
+                    num = random.choice([2, 4])
                     x[i][j] = num
                     ans = True
                     break
-            if (ans):
+            if ans:
                 break
-        if (ans == False):
+        if ans == False:
             random_num()
+
 
 def upper_move():
     add = 0
     for i in range(4):
         for j in range(4):
-            if (x[j][i] == " "):
+            if x[j][i] == " ":
                 for k in range(4):
-                    if (x[k][i] != " " and k > j):
+                    if x[k][i] != " " and k > j:
                         x[j][i] = x[k][i]
                         x[k][i] = " "
                         break
         for j in range(3):
-            if (x[j][i] == x[j + 1][i] and x[j][i] != " "):
+            if x[j][i] == x[j + 1][i] and x[j][i] != " ":
                 x[j][i] = x[j][i] + x[j + 1][i]
                 add += x[j][i]
                 x[j + 1][i] = " "
         for j in range(4):
-            if (x[j][i] == " "):
+            if x[j][i] == " ":
                 for k in range(4):
-                    if (x[k][i] != " " and k > j):
+                    if x[k][i] != " " and k > j:
                         x[j][i] = x[k][i]
                         x[k][i] = " "
                         break
     return add
+
 
 def lower_move():
     add = 0
     for i in range(4):
         for j in range(4):
-            if (x[3 - j][3 - i] == " "):
+            if x[3 - j][3 - i] == " ":
                 for k in range(4):
-                    if (x[3 - k][3 - i] != " " and k > j):
+                    if x[3 - k][3 - i] != " " and k > j:
                         x[3 - j][3 - i] = x[3 - k][3 - i]
                         x[3 - k][3 - i] = " "
                         break
         for j in range(3):
-            if (x[3 - j][3 - i] == x[3 - j - 1][3 - i] and x[3 - j][3 - i] != " "):
+            if x[3 - j][3 - i] == x[3 - j - 1][3 - i] and x[3 - j][3 - i] != " ":
                 x[3 - j][3 - i] = x[3 - j][3 - i] + x[3 - j - 1][3 - i]
                 add += x[3 - j][3 - i]
                 x[3 - j - 1][3 - i] = " "
         for j in range(4):
-            if (x[3 - j][3 - i] == " "):
+            if x[3 - j][3 - i] == " ":
                 for k in range(4):
-                    if (x[3 - k][3 - i] != " " and k > j):
+                    if x[3 - k][3 - i] != " " and k > j:
                         x[3 - j][3 - i] = x[3 - k][3 - i]
                         x[3 - k][3 - i] = " "
                         break
     return add
+
 
 def right_move():
     add = 0
     for i in range(4):
         for j in range(4):
-            if (x[i][3 - j] == " "):
+            if x[i][3 - j] == " ":
                 for k in range(4):
-                    if (x[i][3 - k] != " " and k > j):
+                    if x[i][3 - k] != " " and k > j:
                         x[i][3 - j] = x[i][3 - k]
                         x[i][3 - k] = " "
                         break
         for j in range(3):
-            if (x[i][3 - j] == x[i][3 - j - 1] and x[i][3 - j] != " "):
+            if x[i][3 - j] == x[i][3 - j - 1] and x[i][3 - j] != " ":
                 x[i][3 - j] = x[i][3 - j] + x[i][3 - j - 1]
                 add += x[i][3 - j]
                 x[i][3 - j - 1] = " "
         for j in range(4):
-            if (x[i][3 - j] == " "):
+            if x[i][3 - j] == " ":
                 for k in range(4):
-                    if (x[i][3 - k] != " " and k > j):
+                    if x[i][3 - k] != " " and k > j:
                         x[i][3 - j] = x[i][3 - k]
                         x[i][3 - k] = " "
                         break
     return add
+
 
 def left_move():
     add = 0
     for i in range(4):
         for j in range(4):
-            if (x[i][j] == " "):
+            if x[i][j] == " ":
                 for k in range(4):
-                    if (x[i][k] != " " and k > j):
+                    if x[i][k] != " " and k > j:
                         x[i][j] = x[i][k]
                         x[i][k] = " "
                         break
         for j in range(3):
-            if (x[i][j] == x[i][j + 1] and x[i][j] != " "):
+            if x[i][j] == x[i][j + 1] and x[i][j] != " ":
                 x[i][j] = x[i][j] + x[i][j + 1]
                 add += x[i][j]
                 x[i][j + 1] = " "
         for j in range(4):
-            if (x[i][j] == " "):
+            if x[i][j] == " ":
                 for k in range(4):
-                    if (x[i][k] != " " and k > j):
+                    if x[i][k] != " " and k > j:
                         x[i][j] = x[i][k]
                         x[i][k] = " "
                         break
     return add
 
-while(True):
-    print("[bold white]Your Score Is--------> [/]",score)
+
+while True:
+    print("[bold white]Your Score Is--------> [/]", score)
     direction = input("\nEnter the direction: ")
-    if direction == 'W':
-        score += upper_move()    
-    elif direction == 'S':
+    if direction == "W":
+        score += upper_move()
+    elif direction == "S":
         score += lower_move()
-    elif direction == 'D':
+    elif direction == "D":
         score += right_move()
-    elif direction == 'A':
+    elif direction == "A":
         score += left_move()
     else:
-        if (direction != ""):
+        if direction != "":
             clear()
             game_board()
             print("[bold red]Invalid direction[/]\n")
             continue
 
-    if (not empty()):
+    if not empty():
         print("\n[bold red]YOU LOSE...\n[/]")
         print(f"[bold white]The max value achieved is {get_max()}[/]\n")
         break
-    if (is_win()):
+    if is_win():
         print("\n[bold green]YOU WIN...\n[/]")
         print(f"[bold white]The max value achieved is {get_max()}[/]\n")
         break
     clear()
-    if (direction != ""):
+    if direction != "":
         random_num()
     game_board()
